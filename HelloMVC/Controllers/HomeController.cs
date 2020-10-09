55b8be6a65f1f9164f0b372c4df9257285d67def
angelbetance999 <joseangel.mendoza@gmail.com>
2020-10-09 15:37:31 -0600

    AM - Scaffolding Model

--- HelloMVC/Controllers/HomeController.cs
+++ HelloMVC/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,6 +10,24 @@ namespace HelloMVC.Controllers
 {
     public class HomeController : Controller
     {
+        ObjectCache cache = MemoryCache.Default;
+        List<Customer> customers;
+
+        public HomeController()
+        {
+            customers = cache["customers"] as List<Customer>;
+            if (customers == null)
+            {
+                customers = new List<Customer>();
+            }
+
+        }
+
+        public void SaveCache()
+        {
+            cache["customers"] = customers;
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -30,15 +49,16 @@ namespace HelloMVC.Controllers
             return View();
         }
 
-        public ActionResult ViewCustomer(Customer postedCustomer)
+        public ActionResult ViewCustomer(string id)
         {
-            Customer customer = new Customer();
-
-            customer.Id = Guid.NewGuid().ToString();
-            customer.Name = postedCustomer.Name;
-            customer.Phone = postedCustomer.Phone;
-
-            return View(customer);
+            Customer customer = customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            } else
+            {
+                return View(customer);
+            }
         }
 
         public ActionResult AddCustomer() 
@@ -46,12 +66,77 @@ namespace HelloMVC.Controllers
             return View();
         }
 
+        [HttpPost] 
+        public ActionResult AddCustomer(Customer customer)
+        {
+            customer.Id = Guid.NewGuid().ToString();
+            customers.Add(customer);
+            SaveCache();
+
+            return RedirectToAction("CustomerList");
+        }
+
+        public ActionResult EditCustomer(string id) 
+        {
+            Customer customer = customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult EditCustomer(Customer customer, string id)
+        {
+            var customerToEdit = customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                customerToEdit.Name = customer.Name;
+                customerToEdit.Phone = customer.Phone;
+                SaveCache();
+                return RedirectToAction("CustomerList");
+            }
+        }
+
+        public ActionResult DeleteCustomer(string id)
+        {
+            Customer customer = customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("DeleteCustomer")]
+        public ActionResult ConfirmDeleteCustomer(string id)
+        {
+            Customer customer = customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                customers.Remove(customer);
+                return RedirectToAction("CustomerList");
+            }
+        }
+
         public ActionResult CustomerList() 
         {
-            List<Customer> customers = new List<Customer>();
-            customers.Add(new Customer() { Name = "Angel Mendoza - Movil", Phone = "(915) 603-2052" });
-            customers.Add(new Customer() { Name = "Angel Mendoza - Work",  Phone = "(915) 600-1116" });
-
             return View(customers);
         }
     }
